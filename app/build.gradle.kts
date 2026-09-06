e3c2842661946ba5a2b998d4385859493c7c6b2e
SHIGUANYA <3317611523@qq.com>
2026-09-06 18:09:06 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
-    compileOnly("de.robv.android.xposed:api:83")  // ★ 改为 83
+    compileOnly("de.robv.android.xposed:api:87")  // ★ 改为 87（存在的版本）
 }
