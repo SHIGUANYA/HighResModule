4457dfa44aec52985f82284a67c3bf707fdfdf19
SHIGUANYA <3317611523@qq.com>
2026-09-06 16:41:30 +0800

    Update HighResModule.kt

--- app/src/main/java/com/hook/highres/HighResModule.kt
+++ app/src/main/java/com/hook/highres/HighResModule.kt
@@ -13,36 +13,42 @@ class HighResModule : IXposedHookLoadPackage {
 
         XposedBridge.log("[HighResModule] 高能英雄已加载，开始 Hook 分辨率")
 
-        XposedHelpers.findAndHookMethod(
-            "com.tencent.fastpicture.FastPictureModule",
-            lpparam.classLoader,
-            "Init",
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
-                    XposedBridge.log("[HighResModule] FastPictureModule.Init 被调用，强制分辨率")
-                    try {
-                        val engineClass = XposedHelpers.findClass("com.epicgames.ue4.GameEngine", lpparam.classLoader)
-                        val cvarManager = XposedHelpers.callStaticMethod(engineClass, "GetConsoleVariableManager")
-                        if (cvarManager != null) {
-                            XposedHelpers.callMethod(cvarManager, "SetCVar", "fp.DefaultRenderLevel", 4)
-                            XposedHelpers.callMethod(cvarManager, "SetCVar", "fp.MaxSupportRenderLevel", 6)
-                            XposedHelpers.callMethod(cvarManager, "SetCVar", "r.MobileContentScaleFactor", 2.0f)
-                            XposedBridge.log("[HighResModule] 强制设置: Level 4, MaxLevel 6, Scale 2.0")
+        // Hook FastPictureModule.Init
+        try {
+            XposedHelpers.findAndHookMethod(
+                "com.tencent.fastpicture.FastPictureModule",
+                lpparam.classLoader,
+                "Init",
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
+                        XposedBridge.log("[HighResModule] FastPictureModule.Init 被调用，强制分辨率")
+                        try {
+                            val engineClass = XposedHelpers.findClass("com.epicgames.ue4.GameEngine", lpparam.classLoader)
+                            val cvarManager = XposedHelpers.callStaticMethod(engineClass, "GetConsoleVariableManager")
+                            if (cvarManager != null) {
+                                XposedHelpers.callMethod(cvarManager, "SetCVar", "fp.DefaultRenderLevel", 4)
+                                XposedHelpers.callMethod(cvarManager, "SetCVar", "fp.MaxSupportRenderLevel", 6)
+                                XposedHelpers.callMethod(cvarManager, "SetCVar", "r.MobileContentScaleFactor", 2.0f)
+                                XposedBridge.log("[HighResModule] 强制设置: Level 4, MaxLevel 6, Scale 2.0")
+                            }
+                        } catch (e: Throwable) {
+                            XposedBridge.log("[HighResModule] 反射异常: ${e.message}")
                         }
-                    } catch (e: Throwable) {
-                        XposedBridge.log("[HighResModule] 反射异常: ${e.message}")
                     }
                 }
-            }
-        )
+            )
+        } catch (e: Throwable) {
+            XposedBridge.log("[HighResModule] FastPictureModule hook 注册失败: ${e.message}")
+        }
 
+        // 备选：Hook GameEngine.initialize
         try {
             XposedHelpers.findAndHookMethod(
                 "com.epicgames.ue4.GameEngine",
                 lpparam.classLoader,
                 "initialize",
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
+                object : de.robv.android.xposed.XC_MethodHook() {
+                    override fun afterHookedMethod(param: de.robv.android.xposed.XC_MethodHook.MethodHookParam) {
                         try {
                             val engineClass = XposedHelpers.findClass("com.epicgames.ue4.GameEngine", lpparam.classLoader)
                             val cvarManager = XposedHelpers.callStaticMethod(engineClass, "GetConsoleVariableManager")
