af7a7fe92d7d375184ae0c5bdfe9bd85c30212de
SHIGUANYA <3317611523@qq.com>
2026-09-06 18:52:16 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,9 @@
 pluginManagement {
     repositories {
-        // Google Maven 必须第一位，且显式写 URL
-        maven { url = uri("https://maven.google.com") }
-        // 其它仓库
         gradlePluginPortal()
+        google()
         mavenCentral()
+        maven { url = uri("https://maven.google.com") }
     }
 }
 rootProject.name = "HighResModule"
