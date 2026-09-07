175c0310e1b7c84371c01ed6496e3d27a18d32ea
HighRes Module <highres@hook.local>
2026-09-07 19:26:14 +0800

    Restore LOG_TAG define for JNI build

--- app/src/main/cpp/native_helper.cpp
+++ app/src/main/cpp/native_helper.cpp
@@ -2,6 +2,7 @@
 #include <jni.h>
 #include <android/log.h>
 #include <dlfcn.h>
+#define LOG_TAG "HighResNative"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #else
